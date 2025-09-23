d69f49915129d8e568bcdbddef82df1b858ef467
Rishirk2231 <resikumarpattabiraman@gmail.com>
2025-09-23 21:24:54 +0530

    Update New.java

--- src/test/java/parallelsteps/New.java
+++ src/test/java/parallelsteps/New.java
@@ -3,6 +3,7 @@ package parallelsteps;
 public class New {
 	
 	public static void main(String[] args) {
+		System.out.println("Changes made from remote repository");
 		System.out.println("New method ");
 	}
 
